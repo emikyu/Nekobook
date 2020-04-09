95de988fa814a645f716cbb39d67cb9a838c9f8d
Emily Wu <huijiawu90@gmail.com>
2020-04-08 23:33:45 -0400

    Fully configured AWS and ActiveStorage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_09_022724) do
+ActiveRecord::Schema.define(version: 2020_04_09_031913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,13 +47,11 @@ ActiveRecord::Schema.define(version: 2020_04_09_022724) do
     t.string "password_digest", null: false
     t.string "session_token", null: false
     t.string "fname", null: false
-    t.string "profile_picture"
     t.integer "location_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "birthday", null: false
     t.string "gender", null: false
-    t.string "cover_photo"
     t.string "username"
     t.string "lname", null: false
     t.index ["email"], name: "index_nekos_on_email", unique: true
