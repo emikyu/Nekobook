57e4ed7902a778fbb82ea8ffda973b789bdf7458
Emily Wu <huijiawu90@gmail.com>
2020-04-15 18:10:52 -0400

    Fixed small visual bug in coverpage button

--- frontend/components/newsfeed/newsfeed.jsx
+++ frontend/components/newsfeed/newsfeed.jsx
@@ -3,21 +3,24 @@ import NewsfeedPostFormContainer from '../posts/newsfeed_post_form_container';
 import NewsfeedPostIndexContainer from '../posts/newsfeed_post_index_container';
 
 
-const Newsfeed = () => (
-    <section className="newsfeed">
-        <section className="newsfeed-container">
-            <nav className="newsfeed-nav">
-                Hello from the newsfeed nav menu!
-            </nav>
-            <section className="newsfeed-content">
-                <NewsfeedPostFormContainer />
-                <NewsfeedPostIndexContainer />
-            </section>
-            <section className="newsfeed-side">
-                Hello from the newsfeed side panel :D
+const Newsfeed = () => {
+
+    return (
+        <section className="newsfeed">
+            <section className="newsfeed-container">
+                <nav className="newsfeed-nav">
+                    Hello from the newsfeed nav menu!
+                </nav>
+                <section className="newsfeed-content">
+                    <NewsfeedPostFormContainer />
+                    <NewsfeedPostIndexContainer />
+                </section>
+                <section className="newsfeed-side">
+                    Hello from the newsfeed side panel :D
+                </section>
             </section>
         </section>
-    </section>
-);
+    )
+};
 
 export default Newsfeed;
